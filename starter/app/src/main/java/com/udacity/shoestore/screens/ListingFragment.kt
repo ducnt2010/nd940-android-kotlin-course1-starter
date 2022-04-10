389b1a5bb0f0d0e21bc80b37a898c63c27c502d4
ducnt2010 <mrduca2ltt@gmail.com>
2022-04-10 18:01:43 +0700

    add base content fragments

---
+++ starter/app/src/main/java/com/udacity/shoestore/screens/ListingFragment.kt
@@ -0,0 +1,27 @@
+package com.udacity.shoestore.screens
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.ListingFragmentBinding
+
+class ListingFragment : Fragment() {
+    private lateinit var binding: ListingFragmentBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.listing_fragment,
+            container,
+            false
+        )
+        return binding.root
+    }
+}
